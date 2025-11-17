5bed7e095009e4284f8f7053531f1dfaab54c658
badasswp <badasswpdev@gmail.com>
2025-11-17 14:37:23 +0100

    refactor: use Track component for render

--- src/screens/Presets.tsx
+++ src/screens/Presets.tsx
@@ -1,9 +1,11 @@
 import React, { JSX } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, Pressable } from 'react-native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
+import { Track } from '../components/All';
 import { APP_THEME_COLOR } from '../utils/constants';
+import { tracks } from '../utils/data';
 
 type RootStackParamList = {
 	Home: undefined;
@@ -23,16 +25,30 @@ type PresetsScreenNavigationProp =
 const Presets = (): JSX.Element => {
 	const navigation = useNavigation<PresetsScreenNavigationProp>();
 
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<Pressable onPress={() => navigation.goBack()}>
+				<Text>Back</Text>
+			</Pressable>
+			<ScrollView
+				alwaysBounceVertical={true}
+				showsVerticalScrollIndicator={false}
+			>
+				{tracks &&
+					tracks.map(({ name, bpm }, index) => (
+						<Track key={index} name={name} bpm={bpm}></Track>
+					))}
+			</ScrollView>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: APP_THEME_COLOR,
-		alignItems: 'center',
-		justifyContent: 'center',
 		gap: 20,
+		paddingVertical: 100,
 	},
 });
 
