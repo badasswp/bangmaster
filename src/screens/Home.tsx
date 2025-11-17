4123783594ac1125f7044d2eabef99cd81f4043b
badasswp <badasswpdev@gmail.com>
2025-11-17 10:47:04 +0100

    feat: implement Home screen

---
+++ src/screens/Home.tsx
@@ -0,0 +1,146 @@
+import React, { JSX, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, View, Text } from 'react-native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Audio } from 'expo-av';
+
+import { Button } from '../components/All';
+import { getAppFont } from '../utils/fonts';
+import { APP_THEME_COLOR } from '../utils/constants';
+import { tracks } from '../utils/data';
+
+type RootStackParamList = {
+	Home: undefined;
+};
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList>;
+
+/**
+ * Home screen.
+ *
+ * This screen is used to display
+ * the landing screen for the app.
+ *
+ * @returns {JSX.Element} The Home screen.
+ */
+const Home = (): JSX.Element => {
+	const navigation = useNavigation<HomeScreenNavigationProp>();
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [track, setTrack] = useState<Audio.Sound | null>();
+	const [selection, setSelection] = useState<number>(0);
+
+	const stopPlay = () => {
+		track?.pauseAsync();
+		setIsPlaying(false);
+	};
+
+	const restartPlay = async () => {
+		stopPlay();
+		await handlePlay();
+	};
+
+	const handlePlay = async () => {
+		if (isPlaying) {
+			stopPlay();
+			return;
+		}
+
+		const { sound } = await Audio.Sound.createAsync(tracks[selection].beat, {
+			rate: 1.0,
+			isLooping: true,
+			volume: 1.0,
+		});
+
+		await sound.playAsync();
+
+		setTrack(sound);
+		setIsPlaying(true);
+	};
+
+	const handlePrev = async () => {
+		setSelection(selection => selection - 1);
+		await restartPlay();
+	};
+
+	const handleNext = async () => {
+		setSelection(selection => selection + 1);
+		await restartPlay();
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.trackDetails}>
+				<Text style={styles.trackName}>{tracks[selection].name}</Text>
+				<Text style={styles.trackDuration}>{tracks[selection].duration}</Text>
+			</View>
+			<View style={styles.trackButtons}>
+				<Button text="-" onClick={handlePrev} />
+				<Button
+					text={isPlaying ? 'Stop' : 'Play'}
+					iconStyles={styles.playIcon}
+					style={styles.playButton}
+					onClick={handlePlay}
+				/>
+				<Button text="+" onClick={handleNext} />
+			</View>
+			<View style={styles.trackDetails}>
+				<Text style={styles.bpm}>{tracks[selection].bpm} bpm</Text>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: APP_THEME_COLOR,
+		alignItems: 'center',
+		justifyContent: 'center',
+		gap: 20,
+	},
+
+	trackDetails: {
+		gap: 5,
+	},
+
+	trackName: {
+		fontFamily: getAppFont('400'),
+		color: '#fff',
+		fontSize: 32,
+		fontWeight: 700,
+		textAlign: 'center',
+	},
+
+	trackDuration: {
+		fontFamily: getAppFont('400'),
+		color: 'orange',
+		fontSize: 20,
+		textAlign: 'center',
+	},
+
+	trackButtons: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 10,
+	},
+
+	playIcon: {
+		width: 50,
+		height: 50,
+	},
+
+	playButton: {
+		width: 160,
+		height: 160,
+	},
+
+	bpm: {
+		fontFamily: getAppFont('300'),
+		color: '#fff',
+		fontSize: 56,
+		fontWeight: 300,
+		textAlign: 'center',
+	},
+});
+
+export default Home;
