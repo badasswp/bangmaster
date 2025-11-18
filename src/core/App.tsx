af25b3cea6c5539b12f0844844c4040dbf6ce907
badasswp <badasswpdev@gmail.com>
2025-11-18 07:20:12 +0100

    feat: use redux store

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -2,6 +2,9 @@ import React, { JSX } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { store } from '../store';
+import { Provider } from 'react-redux';
+
 import { Home, Presets, Settings } from '../screens/All';
 
 /**
@@ -19,18 +22,20 @@ const App = (): JSX.Element => {
 	const Stack = createNativeStackNavigator();
 
 	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				initialRouteName="Home"
-				screenOptions={{
-					headerShown: false,
-				}}
-			>
-				<Stack.Screen name="Home" component={Home} />
-				<Stack.Screen name="Presets" component={Presets} />
-				<Stack.Screen name="Settings" component={Settings} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<Provider store={store}>
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName="Home"
+					screenOptions={{
+						headerShown: false,
+					}}
+				>
+					<Stack.Screen name="Home" component={Home} />
+					<Stack.Screen name="Presets" component={Presets} />
+					<Stack.Screen name="Settings" component={Settings} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</Provider>
 	);
 };
 
