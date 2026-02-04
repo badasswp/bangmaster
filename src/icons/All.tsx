c0d4a48c981200dc8cbec2ad2e62dab2e6432fed
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-02-04 09:53:44 +0100

    feat: add angleBack icon to all exported icons

--- src/icons/All.tsx
+++ src/icons/All.tsx
@@ -3,5 +3,6 @@ import Presets from './Presets';
 import Settings from './Settings';
 import Stop from './Stop';
 import Search from './Search';
+import AngleBack from './AngleBack';
 
-export { Play, Presets, Settings, Stop, Search };
+export { Play, Presets, Settings, Stop, Search, AngleBack };
