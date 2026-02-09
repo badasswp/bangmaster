6cf316f0522b4bdae01e56c8b085df8f0c67b220
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-02-09 15:56:02 +0100

    feat: implement header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,81 @@
+import React, { JSX } from 'react';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+import Icon from './Icon';
+import { APP_FONT, APP_FONT_SIZE } from '../utils/constants';
+
+type RootStackParamList = {
+	Notifications: undefined;
+};
+
+type NotificationsScreenNavigationProp = NativeStackNavigationProp<
+	RootStackParamList,
+	'Notifications'
+>;
+
+/**
+ * Header component.
+ *
+ * This component is used for displaying
+ * the top view of the screens and views.
+ *
+ * It contains the Icon component displayed at the far right side.
+ *
+ * @returns {JSX.Element} The Header component.
+ */
+const Header = (): JSX.Element => {
+	const navigation = useNavigation<NotificationsScreenNavigationProp>();
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.bangMasterCaption}>Bangmaster</Text>
+
+			<View>
+				<Pressable onPress={() => navigation.goBack()} testID="searchButton">
+					<Icon
+						name="search"
+						color="white"
+						containerStyles={styles.navIcon}
+						weight={2}
+					/>
+				</Pressable>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		width: '100%',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		backgroundColor: '#2d2b43',
+		padding: 20,
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		zIndex: 10,
+	},
+
+	bangMasterCaption: {
+		color: 'white',
+		fontFamily: APP_FONT,
+		fontSize: 18, //APP_FONT_SIZE,
+		fontWeight: '700',
+		textAlign: 'center',
+		width: 'auto',
+		lineHeight: APP_FONT_SIZE,
+		letterSpacing: -1,
+	},
+
+	navIcon: {
+		alignItems: 'flex-end',
+		color: 'white',
+	},
+});
+
+export default Header;
